acaa1dfd621053664cde4046a4c179a46bcfc9b9
Bryan Szendel <bryan.szendel@gmail.com>
2019-08-08 19:49:01 -0600

    App.test.js tests are passing simulating a click

--- app/src/App.js
+++ app/src/App.js
@@ -9,15 +9,44 @@ function App() {
   const [strikes, setStrikes] = useState(0)
   const [balls, setBalls] = useState(0)
 
+  const strikeCounter = () => {
+    if (strikes + 1 >= 3) {
+      setStrikes(0);
+      setBalls(0)
+    }
+    else setStrikes(strikes +1);
+  }
+  
+  const ballCounter = () => {
+    if (balls +1 >= 4) {
+      setBalls(0);
+      setStrikes(0);
+    }
+    else setBalls(balls +1);
+  }
+  
   const hit = () => {
     setStrikes(0)
     setBalls(0)
   };
 
+  const foul = () => {
+    if ( strikes < 2 ) setStrikes(strikes +1);
+    else if (balls < 3) setBalls(balls +1);
+  }
+  
+
   return (
     <div className="App">
       <Display balls={balls} strikes={strikes}/>
-      <Dashboard hit={hit} balls={balls} strikes={strikes} setBalls={setBalls} setStrikes={setStrikes}/>
+      <Dashboard 
+        hit={hit} 
+        balls={balls} 
+        strikes={strikes} 
+        ballCounter={ballCounter} 
+        strikeCounter={strikeCounter}
+        foul={foul}
+      />
     </div>
   );
 }
--- app/src/Dashboard.js
+++ app/src/Dashboard.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { strikeCounter, ballCounter } from "./helpers";
 
 // balls and strikes reset to 0 when a player reaches 3 strikes or 4 balls.
 // balls and strikes reset to 0 when a `hit` is recorded.
 // a `foul` increases strikes up to 2. With no strikes, a foul makes it 1 strike. With 1 strike, a foul makes it 2 strikes. With two strikes a foul has no effect, count stays at 2 strikes.
 
-const Dashboard = ({balls, strikes, setBalls, setStrikes, hit}) => {
+const Dashboard = ({balls, strikes, setBalls, setStrikes, hit, foul, strikeCounter, ballCounter}) => {
   
   return (
     <div>
-      <button onClick={() => setStrikes(strikeCounter(strikes))}>Add Strike</button>
-      <button onClick={() => setBalls(ballCounter(balls))}>Add Ball</button>
-      <button onClick={() => hit()}>HIT</button>
+      <button onClick={() => strikeCounter()}>Add Strike</button>
+      <button onClick={() => ballCounter()}>Add Ball</button>
+      <button onClick={() => foul()}>Foul</button>
+      <button onClick={() => hit()}>BASE HIT</button>
     </div>
   )
 }
